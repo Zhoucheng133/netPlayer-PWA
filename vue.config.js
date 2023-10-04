a55b09557207805c5d888c83bb6788bb0e81ae7d
周诚 <zhoucheng133@outlook.com>
2023-10-04 21:03:00 +0800

    添加PWA支持

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // TODO 需要修改下面的参数
+  pwa: {
+    name: 'netPlayer PWA',
+    themeColor: '#4c89fe',
+    msTileColor: '#4c89fe',
+    manifestOptions: {
+      start_url: '.',
+      background_color: '#4c89fe'
+    },
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+    }
+  }
 })
